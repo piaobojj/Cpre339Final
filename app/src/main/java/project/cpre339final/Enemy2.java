e3f54a6ef7585c4b1aea38abc4fb1ede4acc6ba4
hsu430 <hsu430@iastate.edu>
2016-12-03 08:32:21 -0600

    All Requirement  Class Element

---
+++ app/src/main/java/project/cpre339final/Enemy2.java
@@ -0,0 +1,8 @@
+package project.cpre339final;
+
+/**
+ * Created by user on 2016/12/3.
+ */
+
+public class Enemy2 {
+}
--- app/src/main/java/project/cpre339final/GameActivity.java
+++ app/src/main/java/project/cpre339final/GameActivity.java
@@ -9,5 +9,6 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
     }
 }
