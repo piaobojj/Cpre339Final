69d5dccd44037d0a3c794cb4d429b6bb467faa96
hsu430 <hsu430@iastate.edu>
2016-12-04 07:54:12 -0600

    Game Almost done

---
+++ app/src/main/java/project/cpre339final/MainActivity.java
@@ -0,0 +1,45 @@
+package project.cpre339final;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button start;
+    Button exit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.startBtn);
+        exit = (Button) findViewById(R.id.exitBtn);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, GameActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+    }
+}
